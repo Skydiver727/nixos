0b66c261233c2282a7621979629d15d1bcb1022e
Skydiver727 <renvelations25@gmail.com>
2025-07-15 11:19:52 +0800

    add env variable
    set ngl in GSK_RENDERER

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,11 @@
   #  wget
   ];
 
+# Fix Render for nixos 25.05
+  environment.variables = {
+  GSK_RENDERER = "ngl";
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
